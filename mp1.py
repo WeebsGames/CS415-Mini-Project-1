7d7b8fc51a6e98428482d0c84972f52c243c0be8
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-02-16 17:39:22 -0600

    fixed median filter

--- mp1.py
+++ mp1.py
@@ -22,10 +22,9 @@ a = np.array((1,2,3))
 print(type(a))
 print(a.shape)
 print(a[0], a[1], a[2])
-
-img = cv.imread(r'lena.png', cv.IMREAD_GRAYSCALE)
-print(len(img))
-px = img[100,100]
+test = cv.imread(r'seele.png', cv.IMREAD_GRAYSCALE)
+print(len(test))
+px = test[100,100]
 print(px)
 kern = np.array([
   [0, -1, 0],
@@ -33,7 +32,7 @@ kern = np.array([
   [0, -1, 0]
 ])
 print(kern)
-print(img[0,0])
+print(test[0,0])
 
 bigkern = np.array([
     [0,0,-1,0,0],
@@ -56,9 +55,9 @@ biggestkern = np.array([
 def convolution(img, kern):
     print("starting convolution")
     k = (int)(len(kern)/2)
-    result = np.zeros((len(img), len(img)))
+    result = np.zeros((len(img), len(img[0])))
     for i in range(len(img)):
-        for j in range(len(img)):
+        for j in range(len(img[0])):
             for u in range(-k, k+1):
                 for v in range(-k,k+1):
                     if(i-u < 0 or j-v < 0 or i-u >= len(img) or j-v >= len(img)):
@@ -67,47 +66,62 @@ def convolution(img, kern):
                         result[i, j] = kern[u, v] * img[i-u, j-v]
     return result
 
-def meanfilter(img, kern):
+def meanfilter(img, size):
     print("starting mean filtering")
-    k = (int)(len(kern)/2)
-    result = np.zeros((len(img), len(img)))
+    k = (int)(size/2)
+    result = np.zeros((len(img), len(img[0])))
     for i in range(len(img)):
-        for j in range(len(img)):
+        for j in range(len(img[0])):
             for u in range(-k, k+1):
                 for v in range(-k,k+1):
-                    if(i-u < 0 or j-v < 0 or i-u >= len(img) or j-v >= len(img)):
+                    if(i-u < 0 or j-v < 0 or i-u >= len(img) or j-v >= len(img[0])):
                         result[i, j] += 0
                         # print(result[i,j])
                     else:
                         result[i, j] += img[i-u, j-v]
                         # print(result[i,j])
             # print("pixel at " + str(i) + ", "+ str(j) + ": " +str(result[i,j]))
-            result[i, j] = result[i, j]/(len(kern) * len(kern))
+            result[i, j] = result[i, j]/(size * size)
     return result
 
-src = cv.imread(r'lena.png')
-gaussimg = cv.filter2D(src, -1, kern)
+def gaussian_kernel(x, y, sigma=1.0):
+    return np.exp(-np.linalg.norm(x - y)**2 / (2 * sigma**2))
+
+lena = cv.imread(r'lena.png')
+
+gaussimg = cv.filter2D(lena, -1, kern)
 cv.imwrite(r'gaussimg.png',gaussimg)
 
-gaussimg = cv.filter2D(src, -1, bigkern)
+gaussimg = cv.filter2D(lena, -1, bigkern)
 cv.imwrite(r'biggaussimg.png',gaussimg)
 
-gaussimg = cv.filter2D(src, -1, biggestkern)
+gaussimg = cv.filter2D(lena, -1, biggestkern)
 cv.imwrite(r'biggestgaussimg.png',gaussimg)
 
-diffimg = convolution(img, kern)
+diffimg = convolution(test, kern)
 cv.imwrite(r'newimgconv.png',diffimg)
 
 # my convolution does not have the same result as cv.filter2D
 
-meanimg = meanfilter(img, kern)
-cv.imwrite(r'newimgmean.png',meanimg)
+art = cv.imread(r'art.png', cv.IMREAD_GRAYSCALE)
+print(len(art))
+print(len(art[0]))
+meanimg = meanfilter(art, 3)
+cv.imwrite(r'newimgmean3x3.png', meanimg)
 
+meanimg = meanfilter(art, 5)
+cv.imwrite(r'newimgmean5x5.png', meanimg)
+
+meanimg = meanfilter(art, 7)
+cv.imwrite(r'newimgmean7x7.png', meanimg)
+
+meanimg = meanfilter(art, 9)
+cv.imwrite(r'newimgmean9x9.png', meanimg)
 white = np.ones((3,3))
 for i in range(0,3):
     for j in range(0,3):
         white[i,j] = 255
 
 cv.imwrite(r'white.png', white)
-meanimg = meanfilter(white,kern)
+meanimg = meanfilter(white,3)
 cv.imwrite(r'whitemean.png',meanimg)
